192da8ce7ff38c8e3b8d34376d0c583a392b46a8
kaajuunity <72707536+kaajuunity@users.noreply.github.com>
2025-11-22 02:53:49 +0530

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+        // Simple scroll animation for CTA button
+        document.querySelector('.cta-button').addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    
